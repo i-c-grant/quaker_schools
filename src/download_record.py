a97b82a5d7654ec6d37845639e3f2d2e4bc24030
Ian Grant <ian.conway.grant@gmail.com>
2024-01-05 19:15:25 -0500

    Added option to specify max number of pages.

--- src/download_record.py
+++ src/download_record.py
@@ -1,6 +1,4 @@
-import glob
 import tempfile
-from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
@@ -13,7 +11,8 @@ from tqdm import tqdm
 @click.command()
 @click.argument("jp2_zip_url")
 @click.argument("output_path")
-def main(jp2_zip_url, output_path):
+@click.option("--max-pages", default=50)
+def main(jp2_zip_url, output_path, max_pages):
     output_dir = Path(output_path).parent
     zip_name = "temp_archive.zip"
 
@@ -38,16 +37,23 @@ def main(jp2_zip_url, output_path):
                 size = f.write(data)
                 progress_bar.update(size)
 
+        progress_bar.close()
+
         if response.status_code != 200:
             raise Exception("Error downloading zip file.")
 
         with ZipFile(temp_dir / zip_name, "r") as zip_file:
             zip_file.extractall(temp_dir)
 
-        with open(output_path, "wb") as f:
-            f.write(img2pdf.convert(*sorted(temp_dir.glob("**/*.jp2"))))
+        file_paths = sorted(temp_dir.glob("**/*.jp2"))
+        page_groups = [file_paths[i:i+max_pages] for i in range(0, len(file_paths), max_pages)]
 
-    print(f"PDF file created successfully at {output_path}")
+        for i, page_group in enumerate(page_groups):
+            output_file_name = f"{str(output_path).rsplit('.', 1)[0]}_{i + 1}.pdf"
+            with open(output_file_name, "wb") as f:
+                f.write(img2pdf.convert(*page_group))
+
+        print(f"PDF files created successfully.")
 
 
 if __name__ == "__main__":
